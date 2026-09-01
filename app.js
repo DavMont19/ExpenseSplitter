bbb1bde0f267a7b0bc598588b9d8f40930b49c75
DavMont19 <davmontoya6@gmail.com>
2026-09-01 15:59:10 -0500

    Lines added

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const personForm = document.getElementById('personForm');
 const personInput = document.getElementById('personInput');
 const peopleList = document.getElementById('peopleList');
 
-
-const people = ['John', 'Carlos', 'Maria'];
+const paidBySelect = document.getElementById('paidBySelect');
+const people = [];
 
 const render = () => {
     peopleList.innerHTML = '';
+    paidBySelect.innerHTML ='';
 
     people.forEach(person => {
         const li = document.createElement('li');
         li.textContent = person;
         peopleList.appendChild(li)
 
-        
+        const option = document.createElement('option');
+        option.value = person;
+        option.textContent = person;
+        paidBySelect.appendChild(option);
+
+
     })
 }
+
+personForm.addEventListener('submit', event =>{
+    event.preventDefault()
+
+    const name = personInput.value.trim();
+
+    if(!name) return;
+
+    people.push(name);
+    personInput.value ='';
+
+    render();
+})
 render();
